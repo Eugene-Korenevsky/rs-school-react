8a199b6ef4a2f3f595c2daa74ba660f6992c4129
Eugene Korenevsky <kara-91@tut.by>
2021-08-10 20:24:04 +0300

    init: init project

---
+++ react-api/src/app/App.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../style.css';
+
+export default function App(): JSX.Element {
+    return <html>
+        <head>
+            <meta charSet="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+            <title>React Api</title>
+            <link href='style.css' rel='stylesheet' />
+        </head>
+        <body>
+            <header className="header">
+            </header>
+            <main>
+
+            </main>
+            <footer className="footer">
+
+            </footer>
+        </body>
+    </html>
+
+}
